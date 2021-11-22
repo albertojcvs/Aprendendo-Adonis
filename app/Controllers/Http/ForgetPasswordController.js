fb53beea2927ad694d3a0f11440d50f9ade2a951
Alberto Jose <albertojcvs@gmail.com>
2021-11-22 11:45:18 -0300

    feat: add code of upload files

--- app/Controllers/Http/ForgetPasswordController.js
+++ app/Controllers/Http/ForgetPasswordController.js
@@ -1,6 +1,7 @@
 "use strict";
-
+const moment = require("moment");
 const crypton = require("crypto");
+const { updateLocale } = require("moment");
 const User = use("App/Models/User");
 const Mail = use("Mail");
 
@@ -29,10 +30,38 @@ class ForgetPasswordController {
         }
       );
     } catch (err) {
-      console.log(err);
       response
         .status(err.status)
         .send({ error: { message: "Algo de errado aconteceu" } });
+    }
+  }
+  async update({ request, response }) {
+    try {
+      const {token, password} = request.all();
+
+      const user = await User.findByOrFail("token", token);
+
+      const isTokenExpired = moment()
+        .subtract("2", "days")
+        .isAfter(user.token_created_at);
+
+      if (isTokenExpired) {
+        response
+          .status(401)
+          .send({ error: { message: "O token de recuperaçao está experido" } });
+      }
+
+      user.tokean = null
+      user.token_created_at = null
+      user.password = password;
+
+      await user.save();
+
+    } catch (err) {
+      console.log(err);
+      response
+      .status(err.status)
+      .send({ error: { message: "Algo de errado aconteceu ao tentar resetar a senha" } });
     }
   }
 }
--- start/routes.js
+++ start/routes.js
@@ -6,5 +6,11 @@ const Route = use('Route')
 Route.get('/', () => {
 })
 Route.post('/users', 'UserController.store')
+
 Route.post('/sessions','SessionController.store');
+
 Route.post('/password', 'ForgetPasswordController.store');
+Route.put('/password', 'ForgetPasswordController.update');
+
+Route.get('/files/:id', 'FileController.show')
+Route.post('/files', 'FileController.store')
